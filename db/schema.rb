8ee8aad1b966e9643224e190bfc29b14b3788d15
Nicholas Chaffee <nick@nicholas-chaffees-macbook.local>
2009-07-03 11:16:06 -0700

    Changed routes.rb so users-index is default route

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20090622181845) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.binary   "photo"
     t.binary   "photo1"
     t.binary   "photo2"
     t.binary   "photo3"
